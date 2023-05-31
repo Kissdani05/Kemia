2c460e7c89c227b89e3015e837d4f2fc4b3bd369
Kissdani005 <kissdani05@gmail.com>
2023-05-31 09:31:55 +0200

    2. commit

---
+++ ConsoleApp1/Adatok.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+
+    class Adatok
+    {
+        public string év;
+        public string név;
+        public string vegyjel;
+        public int rendszam;
+        public string felfedezo;
+
+        public Adatok(string sor)
+        {
+            string[] darabok = sor.Split(';');
+            this.év = darabok[0];
+            this.név= darabok[1];
+            this.vegyjel = darabok[2];
+            this.rendszam = Convert.ToInt32(darabok[3]);
+            this.felfedezo = darabok[4];
+        }
+    }
+    
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -10,7 +11,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("asd");
+            List<Adatok> lista = new List<Adatok>();
+            using (StreamReader sr = new StreamReader("C:\\Users\\tanulo\\Desktop\\Kiss Dániel\\felfedezesek.csv")) { 
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    string sor = sr.ReadLine();
+                    Adatok sor1 = new Adatok(sor);
+                    lista.Add(sor1);
+                }
+                Console.WriteLine("3. feladat: Elemek száma: "+lista.Count());
+                int okor = 0; 
+                foreach (var évek in lista.Where(x=>x.év=="Ókor"))
+                {
+                    okor++;
+                }
+                Console.WriteLine("4. feladat: Felfedezések száma az ókorban: "+okor);
+            }
+
         }
     }
 }
